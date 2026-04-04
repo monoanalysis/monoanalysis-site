1d4ff033e069d22e98d92356edf0294309137e3f
monoanalysis <monuchouhan833@gmail.com>
2026-04-04 17:21:06 -0400

    Added Google Analytics and fixed footer links

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css';
 import Link from 'next/link';
+import Script from 'next/script';
 
 export const metadata = {
   title: 'Monu | Commodity Markets Analysis',
@@ -9,6 +10,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-5JKG4ZLCP0"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-XXXXXXXXXX');
+          `}
+        </Script>
+      </head>
       <body>
         {/* NAV */}
         <nav className="nav">
